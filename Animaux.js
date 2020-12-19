a7cf82ec3429583a9b2488a7dcd043f8b3748a51
Nami <je191030@ens.uqam.ca>
2020-12-19 01:48:09 -0500

    Version 1.53

--- Animaux.js
+++ Animaux.js
@@ -1,14 +1,23 @@
+let reg = new RegExp( '\,' )
+
+jQuery.validator.addMethod("doesNotContain", function(value, element, param) {
+    return this.optional(element) || !value.search(reg);
+}, "Please specify a different (non-default) value");
+
 $(function validateFrom () {
     $("form[name='form']").validate( {
-        nomAnimal: "required",
-        typeAnimal: "required",
-        ageAnimal: {
-            required: true,
-            ageAnimal: true
-        },
-        ecoleEnfant: {
-            required: true,
-
+        rules: {
+            nomAnimal: {
+                required: true,
+                doesNotContain: true,
+            },
+            typeAnimal: "required",
+            race: "required",
+            ageAnimal: {
+                required: true,
+                ageAnimal: true
+            },
+            descriptionT: "required",
         }
 
     })
